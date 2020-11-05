8056bcf408cf3214cf1cc9508f64a4b4d9279bf2
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-11-06 01:20:10 +0300

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import { Character } from './hero'
+import { Bowerman } from './hero'
+import { Swordsman } from './hero'
+import { Magician } from './hero'
+import { Daemon } from './hero'
+import { Undead } from './hero'
+
+
+console.log(new Character('Monster', 'Daemon'))
+console.log(new Bowerman('Nick'))
+console.log(new Swordsman('Nick'))
+console.log(new Magician('Nick'))
+console.log(new Daemon('Nick'))
+console.log(new Undead('Nick'))
+
